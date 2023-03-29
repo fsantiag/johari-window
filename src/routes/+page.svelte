10af803808f54bedf079ebc929a199efa1292607
Felipe Santiago <jfelipe.sfilho@gmail.com>
2023-03-29 21:01:36 +0200

    add tailwind css to the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>This is a test for continous deployment</p>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.red.100);
+	}
+</style>
