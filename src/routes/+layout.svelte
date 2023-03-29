e273f80b65dd39da22a752c51cfb250c773f1283
Felipe Santiago <jfelipe.sfilho@gmail.com>
2023-03-29 23:04:43 +0200

    create ugly landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
   import "../app.css";
+  import "@fontsource/roboto";
+
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>This is a test for continous deployment</p>
+<div class="flex h-screen">
+	<div class="m-auto">
+		<button class="rounded bg-orange p-2 hover:bg-blue p-3 text-xl">Start</button>
+		<button class="rounded bg-orange p-2 hover:bg-blue p-3 text-xl">Join</button>
+	</div>
+</div>
 
 <style lang="postcss">
 	:global(html) {
-		background-color: theme(colors.red.100);
+		background-color: theme(colors.purple);
 	}
 </style>
