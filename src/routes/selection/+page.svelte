07098bbc74e5d95fac5dfeff51d57ef91e1a0722
Felipe Santiago <jfelipe.sfilho@gmail.com>
2023-04-01 20:58:30 +0200

    add store to save the selected adjectives by the user

--- src/routes/selection/+page.svelte
+++ src/routes/selection/+page.svelte
@@ -1,83 +1,101 @@
-<p>Select the adjectives!</p>
-<div class="relative overflow-x-auto"></div>
-<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-	<tbody class="adjectives">
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">able</td>
-			<td class="px-6 py-4 hover:bg-blue-900">accepting</td>
-			<td class="px-6 py-4 hover:bg-blue-900">adaptable</td>
-			<td class="px-6 py-4 hover:bg-blue-900">bold</td>
-			<td class="px-6 py-4 hover:bg-blue-900">brave</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">calm</td>
-			<td class="px-6 py-4 hover:bg-blue-900">caring</td>
-			<td class="px-6 py-4 hover:bg-blue-900">cheerful</td>
-			<td class="px-6 py-4 hover:bg-blue-900">clever</td>
-			<td class="px-6 py-4 hover:bg-blue-900">complex</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">confident</td>
-			<td class="px-6 py-4 hover:bg-blue-900">dependable</td>
-			<td class="px-6 py-4 hover:bg-blue-900">dignified</td>
-			<td class="px-6 py-4 hover:bg-blue-900">energetic</td>
-			<td class="px-6 py-4 hover:bg-blue-900">extroverted</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">friendly</td>
-			<td class="px-6 py-4 hover:bg-blue-900">giving</td>
-			<td class="px-6 py-4 hover:bg-blue-900">happy</td>
-			<td class="px-6 py-4 hover:bg-blue-900">helpful</td>
-			<td class="px-6 py-4 hover:bg-blue-900">idealistic</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-			><td class="px-6 py-4 hover:bg-blue-900">independent</td>
-			<td class="px-6 py-4 hover:bg-blue-900">ingenious</td>
-			<td class="px-6 py-4 hover:bg-blue-900">intelligent</td>
-			<td class="px-6 py-4 hover:bg-blue-900">introverted</td>
-			<td class="px-6 py-4 hover:bg-blue-900">kind</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">knowledgeable</td>
-			<td class="px-6 py-4 hover:bg-blue-900">logical</td>
-			<td class="px-6 py-4 hover:bg-blue-900">loving</td>
-			<td class="px-6 py-4 hover:bg-blue-900">mature</td>
-			<td class="px-6 py-4 hover:bg-blue-900">modest</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">nervous</td>
-			<td class="px-6 py-4 hover:bg-blue-900">observant</td>
-			<td class="px-6 py-4 hover:bg-blue-900">organised</td>
-			<td class="px-6 py-4 hover:bg-blue-900">patient</td>
-			<td class="px-6 py-4 hover:bg-blue-900">powerful</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">proud</td>
-			<td class="px-6 py-4 hover:bg-blue-900">quiet</td>
-			<td class="px-6 py-4 hover:bg-blue-900">reflective</td>
-			<td class="px-6 py-4 hover:bg-blue-900">relaxed</td>
-			<td class="px-6 py-4 hover:bg-blue-900">religious</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">responsive</td>
-			<td class="px-6 py-4 hover:bg-blue-900">searching</td>
-			<td class="px-6 py-4 hover:bg-blue-900">self-assertive</td>
-			<td class="px-6 py-4 hover:bg-blue-900">self-conscious</td>
-			<td class="px-6 py-4 hover:bg-blue-900">sensible</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">sentimental</td>
-			<td class="px-6 py-4 hover:bg-blue-900">shy</td>
-			<td class="px-6 py-4 hover:bg-blue-900">silly</td>
-			<td class="px-6 py-4 hover:bg-blue-900">spontaneous</td>
-			<td class="px-6 py-4 hover:bg-blue-900">sympathetic</td>
-		</tr>
-		<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-			<td class="px-6 py-4 hover:bg-blue-900">tense</td>
-			<td class="px-6 py-4 hover:bg-blue-900">trustworthy</td>
-			<td class="px-6 py-4 hover:bg-blue-900">warm</td>
-			<td class="px-6 py-4 hover:bg-blue-900">wise</td>
-			<td class="px-6 py-4 hover:bg-blue-900">witty</td>
-		</tr>
-	</tbody>
-</table>
+<script lang="ts">
+	import { selectedAdjectives } from './store';
+
+	let displayAdjectives: string[];
+
+	selectedAdjectives.subscribe((value) => {
+		displayAdjectives = value;
+	});
+
+	const handleClick = (event: MouseEvent) => {
+		const element = event.target as HTMLElement;
+		addAdjective(element)
+	};
+
+	const handleSpace = (event: KeyboardEvent) => {
+		if (event.key === ' ') {
+			const element = event.target as HTMLElement;
+			addAdjective(element);
+		}
+	};
+
+	const addAdjective = (element: HTMLElement) => {
+		element.classList.add('bg-gray-400');
+		selectedAdjectives.update((store) => {
+			const adjective = element.textContent ?? 'unknown';
+			if (!store.includes(adjective)) {
+				store.push(adjective);
+			}
+			return store;
+		});
+	};
+
+	const adjectives = [
+		'able',
+		'accepting',
+		'adaptable',
+		'bold',
+		'brave',
+		'calm',
+		'caring',
+		'cheerful',
+		'clever',
+		'complex',
+		'confident',
+		'dependable',
+		'dignified',
+		'energetic',
+		'extroverted',
+		'friendly',
+		'giving',
+		'happy',
+		'helpful',
+		'idealistic',
+		'independent',
+		'ingenious',
+		'intelligent',
+		'introverted',
+		'kind',
+		'knowledgeable',
+		'logical',
+		'loving',
+		'mature',
+		'modest',
+		'nervous',
+		'observant',
+		'organised',
+		'patient',
+		'powerful',
+		'proud',
+		'quiet',
+		'reflective',
+		'relaxed',
+		'religious',
+		'responsive',
+		'searching',
+		'self',
+		'self',
+		'sensible',
+		'sentimental',
+		'shy',
+		'silly',
+		'spontaneous',
+		'sympathetic',
+		'tense',
+		'trustworthy',
+		'warm',
+		'wise',
+		'witty'
+	];
+</script>
+
+<p>Your selected adjectives</p>
+<p class="mb-3 text-gray-500 dark:text-gray-400">{displayAdjectives}</p>
+
+<div class="grid grid-cols-2 md:grid-cols-6 gap-4">
+	{#each adjectives as adjective, i}
+		<div class="hover:bg-slate-500" on:click={handleClick} on:keydown={handleSpace}>
+			{adjective}
+		</div>
+	{/each}
+</div>
