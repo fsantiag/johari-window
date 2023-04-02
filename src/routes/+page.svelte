b6470f0b3e785556faa9848bb80654e344901e10
renatalourena <renatalourena@gmail.com>
2023-04-02 19:47:18 +0200

    Add johari description and link to start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
-<button class="rounded bg-orange p-2 hover:bg-blue p-3 text-xl">Start</button>
-<button class="rounded bg-orange p-2 hover:bg-blue p-3 text-xl">Join</button>
+<div class="m-32">
+	<p class="mb-3 text-center text-gray-500 dark:text-gray-400">
+		It is necessary to improve self-awareness and personal development among individuals when they are in a group.
+		The ‘Johari’ window model is a convenient method used to achieve this task of understanding and enhancing communication between the members in a group. 
+		American psychologists Joseph Luft and Harry Ingham developed this model in 1955. 
+		The idea was derived as the upshot of the group dynamics in University of California and was later improved by Joseph Luft. 
+		The name ‘Johari’ came from joining their first two names. 
+		This model is also denoted as feedback/disclosure model of self-awareness.
+	</p>
+	<center>
+		<a class="rounded bg-orange p-2 hover:bg-blue p-3 text-xl" href="/start">Start</a>
+	</center>
+</div>
+
 
 <style lang="postcss">
 	:global(html) {
