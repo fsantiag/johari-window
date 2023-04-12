9790fdbae613184d08c092b1e56cd034fa079e0b
renatalourena <renatalourena@gmail.com>
2023-04-13 00:46:03 +0200

    add design to room page

--- src/routes/room/[id]/+page.svelte
+++ src/routes/room/[id]/+page.svelte
@@ -8,8 +8,29 @@
 	roomStore.update(() => data.roomId);
 </script>
 
-<p>Your room id is {data.id}</p>
+<div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+	<div class="w-full max-w-md space-y-8">
+		<div>
+			<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+				This is your room id
+			</h2>
+			<p class="mt-2 text-center text-sm text-gray-600">
+				<span class="font-medium text-teal-600 hover:text-indigo-500">/room/{data.id}</span>
+			</p>
+		</div>
+	</div>
+</div>
 
-{#each data.participants as participant}
-	<div><a href="/selection/{participant.id}">{participant.name}</a></div>
-{/each}
+<div class="mx-auto max-w-7xl px-6 lg:px-8">
+	<dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+		{#each data.participants as participant}
+			<div class="mx-auto flex max-w-xs flex-col gap-y-4">
+				<a class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-xl"
+					href="/selection/{participant.id}">
+					{participant.name}
+				</a>
+			</div>
+		{/each}
+	</dl>
+</div>
+
