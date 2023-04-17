a45410b437cd73e33423a52acac2236befa1d085
renatalourena <renatalourena@gmail.com>
2023-04-17 18:21:01 +0200

    Add design to result page

--- src/routes/room/[id]/participant/[participantId]/evaluation/+page.svelte
+++ src/routes/room/[id]/participant/[participantId]/evaluation/+page.svelte
@@ -14,17 +14,23 @@
         recognizedByOthersOnly
     } = useAdjectiveEvaluation(myself, evaluations);
 </script>
+<div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+      <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+        <dt class="text-base leading-7 text-gray-600">{recognizedByOthersAndMe.join(", ")}</dt>
+		<dd class="tooltip order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Self Aware</dd>
+      </div>
 
-<div class="p-3">YOUR EVALUATION</div>
-<div class="p-3">
-	myself: {data.participantId}
-</div>
-<div class="p-3">
-	Recoginized by others and me: {JSON.stringify(recognizedByOthersAndMe)}
-</div>
-<div class="p-3">
-	Recoginized by me only: {JSON.stringify(recognizedByMeOnly)}
-</div>
-<div class="p-3">
-	Recoginized by others only: {JSON.stringify(recognizedByOthersOnly)}
+      <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+        <dt class="text-base leading-7 text-gray-600">{recognizedByMeOnly.join(", ")}</dt>
+        <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Hidden Area</dd>
+      </div>
+
+      <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+        <dt class="text-base leading-7 text-gray-600">{recognizedByOthersOnly.join(", ")}</dt>
+        <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Blind Spot</dd>
+      </div>
+    </dl>
+  </div>
 </div>
