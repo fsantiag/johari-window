68192cc1a85456600f0b436bbd3a5639a014aa32
renatalourena <renatalourena@gmail.com>
2023-04-17 19:07:49 +0200

    Fix select/unselect of adjectives

--- src/routes/selection/[id]/AdjectiveMatrix.svelte
+++ src/routes/selection/[id]/AdjectiveMatrix.svelte
@@ -7,10 +7,16 @@
 
 	let selectedAdjectives: string[] = [];
 
-	const addAdjective = (element: HTMLElement) => {
+	const selectAdjective = (element: HTMLElement) => {
 		const adjective = element.textContent ?? 'unknown';
-		selectedAdjectives.push(adjective);
-		element.classList.add('bg-gray-400');
+		if (selectedAdjectives.includes(adjective)) {
+			selectedAdjectives = selectedAdjectives.filter( item => item !== adjective)
+			element.classList.remove('bg-gray-400');
+		} else {
+			selectedAdjectives.push(adjective);
+			element.classList.add('bg-gray-400');
+		}
+
 		assignedAdjectivesStore.update((store) => {
 			store[participant.id] = selectedAdjectives;
 			return store;
@@ -19,13 +25,13 @@
 
 	const handleClick = (event: MouseEvent) => {
 		const element = event.target as HTMLElement;
-		addAdjective(element);
+		selectAdjective(element);
 	};
 
 	const handleSpace = (event: KeyboardEvent) => {
 		if (event.key === ' ') {
 			const element = event.target as HTMLElement;
-			addAdjective(element);
+			selectAdjective(element);
 		}
 	};
 </script>
@@ -37,14 +43,16 @@
 				<h2 class="text-base font-semibold leading-7 text-gray-900">Adjective Selection</h2>
 				<div class="mt-2">
 					<p class="mt-1 text-sm leading-6 text-gray-600 py-1">
-						Select all adjetives that you believe it could be used to decribe <b>{participant.name}</b>:
+						Select all adjetives that you believe it could be used to decribe <b
+							>{participant.name}</b
+						>:
 					</p>
 					<div class="grid grid-cols-2 md:grid-cols-6 gap-4">
 						{#each ADJECTIVES as adjective, i}
 							<button
-							
 								class=" hover:bg-gray-400 text-sm border-2 border-gray-400 text-black py-1 px-2 rounded"
-								on:click={handleClick} on:keydown={handleSpace}
+								on:click={handleClick}
+								on:keydown={handleSpace}
 							>
 								{adjective}
 							</button>
